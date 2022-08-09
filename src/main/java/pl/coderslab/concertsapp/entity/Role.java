932355a0689d8ca4df73f383f8ab69a754e03ae9
Paw3lUlita <pawel.ulita33@gmail.com>
2022-08-09 08:16:35 +0200

    enties + repositories

---
+++ src/main/java/pl/coderslab/concertsapp/entity/Role.java
@@ -0,0 +1,16 @@
+package pl.coderslab.concertsapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+@Data
+public class Role {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String name;
+}
