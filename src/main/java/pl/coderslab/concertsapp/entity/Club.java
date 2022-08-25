cbaeb2954274383b2ee82d3c1a6703c1c6c53018
Paw3lUlita <pawel.ulita33@gmail.com>
2022-08-25 15:57:55 +0200

    club entity refactoring

--- src/main/java/pl/coderslab/concertsapp/entity/Club.java
+++ src/main/java/pl/coderslab/concertsapp/entity/Club.java
@@ -3,6 +3,7 @@ package pl.coderslab.concertsapp.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -22,6 +23,17 @@ public class Club {
     @NotEmpty(message = "Nazwa miasta nie może być pusta!")
     @Size(min=3, message = "Nazwa miasta musi mieć co najmniej trzy znaki!")
     private String city;
+
+    @NotEmpty(message = "Musisz podac adres!")
+    private String address;
+
+    @NotEmpty(message = "Musisz podać numer telefonu!")
+    private int phoneNumber;
+
+    @Email(message = "Niepoprawny adres email!")
+    @NotEmpty(message = "Musisz podać adres email!")
+    private String email;
+
 
     @ManyToOne
     @JoinColumn(name = "user_id")
